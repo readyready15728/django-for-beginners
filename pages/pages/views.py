b72dd8c44acfefd6156d772d6685539775f091a3
Lynn Bradshaw <readyready15728@gmail.com>
2018-06-17 08:10:37 -0400

    Fixing mistake in view HomePageView in app pages in environment pages

--- pages/pages/views.py
+++ pages/pages/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
-    template_name = 'hello.html'
+    template_name = 'home.html'
